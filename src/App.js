00ec6666eacb5ccd0e7774a8ab9187afc0efbd59
Yusuke Miyazaki <ysk1180@gmail.com>
2019-11-10 17:23:49 +0900

    金額と出発地を入れて、候補地を表示するベースを作るところまで

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { budget: '10000', departure: '二子玉川駅', courses_names: [] }
+
+  onFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await axios.get('http://localhost:3001/search', {
+      params: { budget: this.state.budget, departure: this.state.departure }
+    });
+
+    this.setState({ courses_names: response.data.course_names })
+  }
+
+  render() {
+    const names = this.state.courses_names.map(name => {
+      return <p>{name}</p>
+    });
+
+    return (
+      <div>
+        <div>
+          <form onSubmit={this.onFormSubmit}>
+            <div>
+              <select value={this.state.badget} onChange={e => this.setState({ budget: e.target.value })}>
+                <option value="10000">10000</option>
+                <option value="5000">5000</option>
+              </select>
+            </div>
+            <div>
+              <input type="text" value={this.state.departure} onChange={e => this.setState({ departure: e.target.value })} />
+            </div>
+            <div>
+              <input type="submit" />
+            </div>
+          </form>
+        </div>
+        <div>
+          {names}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
