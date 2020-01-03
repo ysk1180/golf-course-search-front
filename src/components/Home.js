7d627ef470aff9f26bcb5910c41427e46b88c3a5
Yusuke Miyazaki <ysk1180@gmail.com>
2020-01-03 20:56:27 +0900

    検索フォームの内容修正

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ import './Common.css';
 registerLocale('ja', ja);
 
 class Home extends React.Component {
-  state = { date: '', budget: '10000', departure: '1', duration: '90', loading: false, error: '', plans: null, count: 0 }
+  state = { date: '', budget: '12000', departure: '1', duration: '90', loading: false, error: '', plans: null, count: 0 }
 
   componentDidMount() {
     let date = new Date();
@@ -70,23 +70,43 @@ class Home extends React.Component {
                 />
               </div>
               <div className="field">
-                <label>金額</label>
+                <label>上限金額</label>
                 <select className="ui dropdown" name="dropdown" value={this.state.budget} onChange={e => this.setState({ budget: e.target.value })}>
-                  <option value="10000">10000</option>
-                  <option value="5000">5000</option>
+                  <option value="7000">7,000円</option>
+                  <option value="8000">8,000円</option>
+                  <option value="9000">9,000円</option>
+                  <option value="10000">10,000円</option>
+                  <option value="11000">11,000円</option>
+                  <option value="12000">12,000円</option>
+                  <option value="13000">13,000円</option>
+                  <option value="14000">14,000円</option>
+                  <option value="15000">15,000円</option>
+                  <option value="16000">16,000円</option>
                 </select>
               </div>
               <div className="field">
-                <label>場所</label>
+                <label>移動時間計算の出発地点（自宅から近い地点をお選びください）</label>
                 <select className="ui dropdown" name="dropdown" value={this.state.departure} onChange={e => this.setState({ departure: e.target.value })}>
                   <option value="1">二子玉川駅</option>
+                  <option value="2">吉祥寺駅</option>
+                  <option value="3">赤羽駅</option>
+                  <option value="4">錦糸町駅</option>
+                  <option value="5">川崎駅</option>
                 </select>
               </div>
               <div className="field">
-                <label>所要時間</label>
+                <label>車での移動時間の上限</label>
                 <select className="ui dropdown" name="dropdown" value={this.state.duration} onChange={e => this.setState({ duration: e.target.value })}>
-                  <option value="90">90分</option>
                   <option value="60">60分</option>
+                  <option value="70">70分</option>
+                  <option value="80">80分</option>
+                  <option value="90">90分</option>
+                  <option value="100">100分</option>
+                  <option value="110">110分</option>
+                  <option value="120">120分</option>
+                  <option value="130">130分</option>
+                  <option value="140">140分</option>
+                  <option value="150">150分</option>
                 </select>
               </div>
               <div className="Search__Button">
