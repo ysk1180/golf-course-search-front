7d807bb91bb0172f44c33f719598f4611496b4fb
miyazaki <kattyan.08ms@gmail.com>
2020-01-04 16:37:58 +0900

    class→classNameにした

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <div class="ui inverted vertical footer segment">
-        <div class="ui container">
-          <div class="ui stackable inverted divided equal height stackable grid">
-            <div class="seven wide column">
-              <h4 class="ui inverted header">開発者（お問い合わせはDMでお願いします）</h4>
+      <div className="ui inverted vertical footer segment">
+        <div className="ui container">
+          <div className="ui stackable inverted divided equal height stackable grid">
+            <div className="seven wide column">
+              <h4 className="ui inverted header">開発者（お問い合わせはDMでお願いします）</h4>
               <span><a href="https://twitter.com/ysk_pro" target="_blank" rel="noopener noreferrer"><i className="twitter square icon"></i>ゆうすけ</a></span>
               <span><a href="https://twitter.com/rails_java_like" target="_blank" rel="noopener noreferrer"><i className="twitter square icon"></i>宮水</a></span>
             </div>
