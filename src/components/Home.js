f7785750a03f95da0146a7dfa897d917de4c8a16
Yusuke Miyazaki <ysk1180@gmail.com>
2020-01-04 19:48:30 +0900

    リファクタ(コードの記載順変更や同じ処理をまとめるなど)

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 import './css/Common.css';
 import './css/Reset.css';
 import 'semantic-ui-css/semantic.min.css'
+
+import addDays from 'date-fns/addDays';
 import format from 'date-fns/format';
 
 import Header from './layout/Header.js';
@@ -11,24 +13,21 @@ import Footer from './layout/Footer.js';
 import Loading from './layout/Loading.js';
 import Result from './Result.js';
 import Search from './Search.js';
-import addDays from 'date-fns/addDays';
 
 class Home extends React.Component {
   state = { date: addDays(new Date(), 14), budget: '12000', departure: '1', duration: '90', loading: false, error: '', plans: null, planCount: 0, planSortType: 'duration' }
 
   onFormSubmit = async (event) => {
     try {
-      this.setState({ loading: true });
       event.preventDefault();
+      this.setState({ loading: true });
 
       const response = await axios.get('https://ttdbfb2924.execute-api.ap-northeast-1.amazonaws.com/production/fetch-golf-plans?', {
         params: { date: format(this.state.date, 'yyyyMMdd'), budget: this.state.budget, departure: this.state.departure, duration: this.state.duration }
       });
-      this.setState({ planCount: response.data.count })
-      this.setState({ plans: response.data.plans })
+      this.setState({ planCount: response.data.count, plans: response.data.plans })
     } catch (e) {
       this.setState({ error: e })
-      console.log(e)
     }
     this.setState({ loading: false });
   }
@@ -38,7 +37,6 @@ class Home extends React.Component {
   }
 
   planSort = (type, sortOrder) => {
-    this.setState({ planSortType: type})
     let plans = this.state.plans
     plans.sort((plan1, plan2) => {
       if (plan1[type] < plan2[type]) {
@@ -49,7 +47,7 @@ class Home extends React.Component {
       }
       return 0
     })
-    this.setState({ plans: plans })
+    this.setState({ plans: plans, planSortType: type })
   }
 
   render() {
