d6f545d992faa111353f306543310d8ef18d5315
Yusuke Miyazaki <ysk1180@gmail.com>
2020-01-06 20:22:41 +0900

    出発地点に飯田橋駅を追加

--- src/components/Search.js
+++ src/components/Search.js
@@ -50,6 +50,7 @@ const Search = ({ date, budget, departure, duration, onFormSubmit, changeState }
                 <option value="2">吉祥寺駅</option>
                 <option value="3">赤羽駅</option>
                 <option value="4">錦糸町駅</option>
+                <option value="7">飯田橋駅</option>
                 <option value="5">川崎駅（神奈川県）</option>
                 <option value="6">川越駅（埼玉県）</option>
               </select>
