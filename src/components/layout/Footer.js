596e97b40d5c0fe8b2e819c4ba8d323ade9e987a
Yusuke Miyazaki <ysk1180@gmail.com>
2020-09-27 12:36:24 +0900

    フッターにゴルフメドレーのリンクを追加した

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,11 +5,16 @@ const Footer = () => {
     <footer>
       <div className="ui inverted vertical footer segment">
         <div className="ui container">
-          <div className="ui stackable inverted divided equal height stackable grid">
-            <div className="seven wide column">
-              <h4 className="ui inverted header">開発者（お問い合わせはDMでお願いします）</h4>
+          <div className="ui stackable inverted equal height stackable grid">
+            <div className="twelve wide column">
+              <p className="ui inverted">開発者（お問い合わせはDMでお願いします）</p>
               <span><a href="https://twitter.com/ysk_pro" target="_blank" rel="noopener noreferrer"><i className="twitter square icon"></i>ゆうすけ</a></span>
               <span><a href="https://twitter.com/rails_java_like" target="_blank" rel="noopener noreferrer"><i className="twitter square icon"></i>宮水</a></span>
+            </div>
+            <div className="twelve wide column">
+              <p className="ui inverted">
+                <a href="https://golf-medley.com/" target="_blank" rel="noopener noreferrer" style={{ marginLeft: 0 }}>Golf Medley（ゴルフ練習場口コミサイト） <i class="external alternate icon"></i></a>
+              </p>
             </div>
           </div>
         </div>
